cd2dc9a0b5b021c109fae2efeaaeee4b5371bc0e
AQUINRU <russel.aquino@oocl.com>
2021-08-03 23:34:04 +0800

    [PosMachine][Russel]feat convertToItems/getItemDetails

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,33 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        convertToItems(barcodes);
+
         return null;
     }
+
+    private List<Item> convertToItems(List<String> barcodes) {
+        List<String> distinctBarcodes;
+        distinctBarcodes = barcodes.stream().distinct().collect(Collectors.toList());
+
+        List<Item> itemsWithDetails = new ArrayList<>();
+        for (String barcode : distinctBarcodes) {
+            //ItemInfo itemInfo = ItemDataLoader.loadAllItemInfos();
+            //itemsWithDetails.add());
+        }
+
+        return itemsWithDetails;
+    }
+
+    private List<Item> calculateSubTotalAndTotal(List<Item> itemsWithDetails) {
+        //
+
+        return null;
+    }
+
 }
