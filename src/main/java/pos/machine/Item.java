933a34c618e5f53b52547d026bc5f26920862a95
AQUINRU <russel.aquino@oocl.com>
2021-08-06 18:52:54 +0800

    [POS][Russel] feat: modified Item class

--- src/main/java/pos/machine/Item.java
+++ src/main/java/pos/machine/Item.java
@@ -1,20 +1,30 @@
 package pos.machine;
 
-public class Item {
+class Item{
     private final String name;
     private final int price;
     private final int quantity;
     private final int subtotal;
 
-    public Item(String name, int price, int quantity, int subtotal) {
+    public Item(String name, int price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.subtotal = quantity * price;
+        this.subtotal = price * quantity;
+    }
+    public int getQuantity() {
+        return quantity;
     }
 
-    public String getName() { return name; }
-    public int getQuantity() { return quantity; }
-    public int getPrice() { return price; }
-    public int getSubtotal() { return subtotal; }
+    public int getSubtotal(){
+        return subtotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
