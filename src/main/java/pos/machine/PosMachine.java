c52817d133921bdc685ec09d5f5f02c26cdd2919
AQUINRU <russel.aquino@oocl.com>
2021-08-06 19:00:02 +0800

    [POS][Russel] feat: implement loadAllItemsInfo

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,33 +1,28 @@
 package pos.machine;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
-        convertToItems(barcodes);
 
-        return null;
-    }
+    private List<Item> loadAllItemsInfo(List<String> barcodes) {
+        List<Item> itemWithDetails = new ArrayList<Item>();
+        List<String> distinctBarCodes;
+        distinctBarCodes = barcodes.stream()
+                .distinct()
+                .collect(Collectors.toList());
 
-    private List<Item> convertToItems(List<String> barcodes) {
-        List<String> distinctBarcodes;
-        distinctBarcodes = barcodes.stream().distinct().collect(Collectors.toList());
+        for (String barcode : distinctBarCodes){
+            ItemInfo itemInfo = ItemDataLoader.loadAllItemInfos().stream()
+                    .filter( b -> b.getBarcode().equals(barcode))
+                    .findFirst().get();
 
-        List<Item> itemsWithDetails = new ArrayList<>();
-        for (String barcode : distinctBarcodes) {
-            //ItemInfo itemInfo = ItemDataLoader.loadAllItemInfos();
-            //itemsWithDetails.add());
+            itemWithDetails.add(new Item(itemInfo.getName(), itemInfo.getPrice(), Collections.frequency(barcodes,barcode)));
         }
-
-        return itemsWithDetails;
-    }
-
-    private List<Item> calculateSubTotalAndTotal(List<Item> itemsWithDetails) {
-        //
-
-        return null;
+        return itemWithDetails;
     }
 
 }
